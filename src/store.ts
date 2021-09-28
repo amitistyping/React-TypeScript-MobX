5fb8d6a9455ad003844287b875ccaaafdab2140b
Amitoj Singh <amitoj.dev@gmail.com>
2021-09-28 16:59:12 +0530

    intial commit

--- src/store.ts
+++ src/store.ts
@@ -1,26 +1,27 @@
-// Standard interface and functions
+import { makeAutoObservable } from "mobx";
+
 export interface Todo {
   id: number;
   text: string;
   done: boolean;
 }
 
-export const updateTodo = (todos: Todo[], id: number, text: string): Todo[] =>
+const updateTodo = (todos: Todo[], id: number, text: string): Todo[] =>
   todos.map((todo) => ({
     ...todo,
     text: todo.id === id ? text : todo.text,
   }));
 
-export const toggleTodo = (todos: Todo[], id: number): Todo[] =>
+const toggleTodo = (todos: Todo[], id: number): Todo[] =>
   todos.map((todo) => ({
     ...todo,
     done: todo.id === id ? !todo.done : todo.done,
   }));
 
-export const removeTodo = (todos: Todo[], id: number): Todo[] =>
+const removeTodo = (todos: Todo[], id: number): Todo[] =>
   todos.filter((todo) => todo.id !== id);
 
-export const addTodo = (todos: Todo[], text: string): Todo[] => [
+const addTodo = (todos: Todo[], text: string): Todo[] => [
   ...todos,
   {
     id: Math.max(0, Math.max(...todos.map(({ id }) => id))) + 1,
@@ -28,3 +29,27 @@ export const addTodo = (todos: Todo[], text: string): Todo[] => [
     done: false,
   },
 ];
+
+//MobX Class
+class Store {
+  todos: Todo[] = [];
+  newTodo: string = '';
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  addTodo() {
+    this.todos = addTodo(this.todos, this.newTodo);
+    this.newTodo = "";
+  }
+
+  removeTodo(id: number) {
+    this.todos = removeTodo(this.todos, id)
+  }
+
+}
+
+const store = new Store();
+
+export default store;
